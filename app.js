4c0a84dc94d803d68a1449ab291d0b6e1f53e4d5
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2025-12-16 17:46:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -249,6 +249,7 @@ const originalTransforms = new Map();
 const DEFAULT_MODEL_TRANSFORMS = {
     //IFC_ILUX: { position: [-14.08, 0, 0] },
     IFC_EST: { position: [-8.789, 0.4, 22.48] },
+    IFC_SPDA: { position: [0.1, 0, 13.8] },
     IFC_LOG_TEF: { position: [0.16, 0, -0.19], rotation: [0, 90, 0] },
     IFC_ELE: { position: [0.16, 0, -0.19] },
     //IFC_ECX: { position: [-14.08, 0, 0] },
@@ -1344,6 +1345,7 @@ viewer.scene.canvas.canvas.addEventListener('contextmenu', (event) => {
     canvasElement.addEventListener('touchend', endTouch, { passive: false });
     canvasElement.addEventListener('touchcancel', clearTouch, { passive: true });
 })();
+
 
 
 
