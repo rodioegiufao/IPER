439682e2b944cfbb656f3f4a274a0b9bdbabdc66
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2025-12-16 16:04:37 -0400

    Update app.js

--- app.js
+++ app.js
@@ -256,7 +256,7 @@ const DEFAULT_MODEL_TRANSFORMS = {
     IFC_INC: { position: [0.15, 0, -0.15], rotation: [0, 90, 0] },
     IFC_HID: { position: [0.2, 0, 13.9], rotation: [0, 90, 0] },
     IFC_PLU: { position: [0.2, 0, 13.9], rotation: [0, 90, 0] },
-    //IFC_GLP: { position: [13.03, 0, -14.05] },
+    IFC_FOT: { position: [50, 0, 0] },
     //IFC_ARQ: { position: [0.16, 0, 13.9], rotation: [0, 90, 0]  },
     //IFC_EST_SUB: { position: [-41.57, 0.4, 15.5], rotation: [0, 90, 0]  },
     //IFC_CLI_DUT: { position: [13, 0, 0], rotation: [0, 90, 0]  },
@@ -542,6 +542,7 @@ const defaultModels = [
     { id: "IFC_EST_CT", src: "assets/modelo-12.xkt" },
     { id: "IFC_EST_MR", src: "assets/modelo-13.xkt" },
     { id: "IFC_EST_MRC", src: "assets/modelo-14.xkt" },
+    { id: "IFC_FOT", src: "assets/modelo-15.xkt" },
 ];
 
 defaultModels.forEach(loadDefaultModel);
@@ -1339,6 +1340,7 @@ viewer.scene.canvas.canvas.addEventListener('contextmenu', (event) => {
     canvasElement.addEventListener('touchend', endTouch, { passive: false });
     canvasElement.addEventListener('touchcancel', clearTouch, { passive: true });
 })();
+
 
 
 
