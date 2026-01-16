28c69e28bd0932160e9b12f6283689856388a1fe
rodioegiufao <126606600+rodioegiufao@users.noreply.github.com>
2026-01-16 12:00:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ const DEFAULT_MODEL_TRANSFORMS = {
     IFC_SAN: { position: [0.2, 0, 13.9], rotation: [0, 90, 0] },
     IFC_SUB: { position: [2.7, -0.25, 2.68], rotation: [0, 96, 0] },
     IFC_INC: { position: [0.15, 0, -0.15], rotation: [0, 90, 0] },
-    IFC_HID: { position: [0.2, 0, 13.9], rotation: [0, 90, 0] },
+    IFC_HID: { position: [0.15, 0, -0.15], rotation: [0, 90, 0] },
     IFC_PLU: { position: [0.2, 0, 13.9], rotation: [0, 90, 0] },
     IFC_FOT: { position: [0, 0, 13.9], rotation: [0, 90, 0]},
     //IFC_ARQ: { position: [0.16, 0, 13.9], rotation: [0, 90, 0]  },
@@ -2179,6 +2179,7 @@ viewer.scene.canvas.canvas.addEventListener('contextmenu', (event) => {
     canvasElement.addEventListener('touchend', endTouch, { passive: false });
     canvasElement.addEventListener('touchcancel', clearTouch, { passive: true });
 })();
+
 
 
 
